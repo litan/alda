e19f8e45eaa040e42a357d2f4d22664702dec06f
Lalit Pant <pant.lalit@gmail.com>
2022-09-09 08:24:13 +0530

    Print Alda info on startup.

--- player/src/main/kotlin/io/alda/player/Main.kt
+++ player/src/main/kotlin/io/alda/player/Main.kt
@@ -89,7 +89,7 @@ class Run : CliktCommand(
     val player = player()
     log.info { "Starting player..." }
     player.start()
-    println("Music Server Started.")
+    println("Alda (https://alda.io/) Music Server Started.")
 
     Runtime.getRuntime().addShutdownHook(thread(start = false) {
       stateManager!!.stop()
